477c6bf575ed794bafc4d3058642ff8a7648443b
ghoshdebashree <shree19ghosh@gmail.com>
2024-01-15 20:54:05 +0530

    Using ListIterator to find the previous element also.

--- src/ArrayListIterator.java
+++ src/ArrayListIterator.java
@@ -10,14 +10,20 @@ public class ArrayListIterator {
         list.add(40);
         list.add(50);
         list.add(60);
-        Iterator<Integer> itr = list.iterator();
-        {
+        //Iterator<Integer> itr = list.iterator();
+        ListIterator<Integer> integerListIterator = list.listIterator();
+        //{
 //           while(itr.hasNext()){
 //               System.out.println(itr.next());
 //           }
 //        }
-            itr.forEachRemaining(element -> System.out.println(element));
 
+            integerListIterator.forEachRemaining(element -> System.out.println(element)); //USING listIterator
+
+       // }
+        System.out.println("Reverse order iteration :");
+        while(integerListIterator.hasPrevious()){
+            System.out.println(integerListIterator.previous());
         }
 
     }
